19ae417b23f5391a6a064b56e9f5c49780ad546c
George MacRorie <me@georgemac.com>
2023-01-10 12:01:29 +0000

    chore(vite): proxy API requests to localhost:8080

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,11 @@ export default defineConfig({
     },
   },
   envPrefix: 'FLIPT_',
+  server: {
+    proxy: {
+      '/api/v1': 'http://localhost:8080',
+      '/auth/v1': 'http://localhost:8080',
+      '/meta': 'http://localhost:8080',
+    },
+  },
 })

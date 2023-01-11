200334730bbed38d4123bd2db1f26e548dd3fe1c
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-01-11 16:52:35 -0500

    fix: constraint form boolean default value and validation

--- src/components/segments/ConstraintForm.tsx
+++ src/components/segments/ConstraintForm.tsx
@@ -1,7 +1,7 @@
 import { Dialog } from '@headlessui/react';
 import { XMarkIcon } from '@heroicons/react/24/outline';
-import { Form, Formik } from 'formik';
-import React, { useState } from 'react';
+import { Form, Formik, useField, useFormikContext } from 'formik';
+import { useEffect, useState } from 'react';
 import * as Yup from 'yup';
 import Button from '~/components/forms/Button';
 import Input from '~/components/forms/Input';
@@ -17,7 +17,7 @@ import {
   ConstraintStringOperators,
   IConstraint,
   IConstraintBase,
-  Operator
+  NoValueOperators
 } from '~/types/Constraint';
 
 const constraintComparisonTypes = () =>
@@ -29,7 +29,7 @@ const constraintComparisonTypes = () =>
   );
 
 const constraintOperators = (c: string) => {
-  let opts: Record<string, Operator> = {};
+  let opts: Record<string, string> = {};
   switch (ComparisonType[c as keyof typeof ComparisonType]) {
     case ComparisonType.STRING_COMPARISON_TYPE:
       opts = ConstraintStringOperators;
@@ -41,8 +41,92 @@ const constraintOperators = (c: string) => {
       opts = ConstraintBooleanOperators;
       break;
   }
-  return Object.entries(opts);
+  return Object.entries(opts).map(([k, v]) => ({
+    value: k,
+    label: v
+  }));
 };
+
+type InputProps = {
+  name: string;
+  id: string;
+};
+
+function ConstraintOperatorSelect(props: InputProps) {
+  const {
+    values: { type },
+    setFieldValue
+  } = useFormikContext<{ type: string }>();
+
+  const [field] = useField(props);
+
+  // set default value for operator when type changes
+  useEffect(() => {
+    if (type === 'BOOLEAN_COMPARISON_TYPE') {
+      setFieldValue(props.name, 'true');
+      return;
+    }
+    setFieldValue(props.name, 'eq');
+  }, [type, props.name, setFieldValue]);
+
+  return (
+    <Select
+      className="mt-1"
+      {...field}
+      {...props}
+      options={constraintOperators(type)}
+    />
+  );
+}
+
+function ConstraintValueField(props: InputProps) {
+  const [show, setShow] = useState(true);
+  const {
+    values: { type, operator }
+  } = useFormikContext<{ type: string; operator: string }>();
+
+  const [field] = useField({
+    ...props,
+    validate: (value) => {
+      if (!show) {
+        return undefined;
+      }
+
+      // value is required only if shown
+      return value ? undefined : 'Value is required';
+    }
+  });
+
+  // show/hide value field based on operator
+  useEffect(() => {
+    if (type === 'BOOLEAN_COMPARISON_TYPE') {
+      setShow(false);
+      return;
+    }
+    const noValue = NoValueOperators.includes(operator);
+    setShow(!noValue);
+  }, [type, operator]);
+
+  if (!show) {
+    return <></>;
+  }
+
+  return (
+    <div className="space-y-1 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
+      <div>
+        <label
+          htmlFor="value"
+          className="block text-sm font-medium text-gray-900 sm:mt-px sm:pt-2"
+        >
+          Value
+        </label>
+      </div>
+      <div className="sm:col-span-2">
+        <Input {...props} {...field} />
+      </div>
+    </div>
+  );
+}
 
 type ConstraintFormProps = {
   setOpen: (open: boolean) => void;
@@ -57,7 +141,6 @@ export default function ConstraintForm(props: ConstraintFormProps) {
 
   const isNew = constraint === undefined;
   const title = isNew ? 'New Constraint' : 'Edit Constraint';
-  const [hasValue, setHasValue] = useState(true);
 
   const initialValues: IConstraintBase = {
     property: constraint?.property || '',
@@ -87,8 +170,7 @@ export default function ConstraintForm(props: ConstraintFormProps) {
           });
       }}
       validationSchema={Yup.object({
-        property: requiredValidation,
-        value: hasValue ? requiredValidation : Yup.string().notRequired()
+        property: requiredValidation
       })}
     >
       {(formik) => (
@@ -108,7 +190,10 @@ export default function ConstraintForm(props: ConstraintFormProps) {
                   <button
                     type="button"
                     className="text-gray-400 hover:text-gray-500"
-                    onClick={() => setOpen(false)}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setOpen(false);
+                    }}
                   >
                     <span className="sr-only">Close panel</span>
                     <XMarkIcon className="h-6 w-6" aria-hidden="true" />
@@ -146,16 +231,7 @@ export default function ConstraintForm(props: ConstraintFormProps) {
                     className="mt-1"
                     value={formik.values.type}
                     options={constraintComparisonTypes()}
-                    handleChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
-                      formik.handleChange(e);
-                      const option = e.target.options[e.target.selectedIndex];
-                      const show = option.value !== 'BOOLEAN_COMPARISON_TYPE';
-                      // TODO: need to update to valid boolean values if operator is invalid
-                      // if (option.value === "BOOLEAN_COMPARISON_TYPE") {
-                      //   formik.setFieldValue("operator", "true");
-                      // }
-                      setHasValue(show);
-                    }}
+                    handleChange={formik.handleChange}
                   />
                 </div>
               </div>
@@ -169,47 +245,22 @@ export default function ConstraintForm(props: ConstraintFormProps) {
                   </label>
                 </div>
                 <div className="sm:col-span-2">
-                  <Select
-                    name="operator"
-                    id="operator"
-                    className="mt-1"
-                    value={formik.values.operator}
-                    handleChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
-                      formik.handleChange(e);
-                      const option = e.target.options[e.target.selectedIndex];
-                      const show =
-                        option.getAttribute('data-has-value') === 'true';
-                      setHasValue(show);
-                    }}
-                  >
-                    {constraintOperators(formik.values.type).map(([k, v]) => (
-                      <option key={k} value={k} data-has-value={!v.noValue}>
-                        {v.label}
-                      </option>
-                    ))}
-                  </Select>
+                  <ConstraintOperatorSelect id="operator" name="operator" />
                 </div>
               </div>
-              {hasValue && (
-                <div className="space-y-1 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
-                  <div>
-                    <label
-                      htmlFor="value"
-                      className="block text-sm font-medium text-gray-900 sm:mt-px sm:pt-2"
-                    >
-                      Value
-                    </label>
-                  </div>
-                  <div className="sm:col-span-2">
-                    <Input name="value" id="value" />
-                  </div>
-                </div>
-              )}
+              <ConstraintValueField name="value" id="value" />
             </div>
           </div>
           <div className="flex-shrink-0 border-t border-gray-200 px-4 py-5 sm:px-6">
             <div className="flex justify-end space-x-3">
-              <Button onClick={() => setOpen(false)}>Cancel</Button>
+              <Button
+                onClick={(e) => {
+                  e.preventDefault();
+                  setOpen(false);
+                }}
+              >
+                Cancel
+              </Button>
               <Button
                 primary
                 type="submit"

da6bb65204dd2c217f64e5432810c6c8eeda95ff
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-01-17 09:20:23 -0500

    chore: forgot to stage renamed files

--- src/data/hooks/useError.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-import { ErrorContext } from '~/components/ErrorProvider';
-
-export const useError = () => {
-  const { error, setError, clearError } = useContext(ErrorContext);
-  return { error, setError, clearError };
-};

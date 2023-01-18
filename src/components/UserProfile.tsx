04a7c505b16af7bc6332dd1dbdc48ef3ebcb9bee
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-01-18 14:36:29 -0500

    chore: show name on hover

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -15,12 +15,13 @@ export default function UserProfile(props: UserProfileProps) {
   return (
     <Menu as="div" className="relative ml-3">
       <div>
-        <Menu.Button className="flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2">
+        <Menu.Button className="flex max-w-xs items-center rounded-full bg-white text-sm hover:ring-2 hover:ring-white focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2">
           <span className="sr-only">Open user menu</span>
           <img
             className="h-8 w-8 rounded-full"
             src={imgURL}
             alt={name}
+            title={name}
             referrerPolicy="no-referrer"
           />
         </Menu.Button>

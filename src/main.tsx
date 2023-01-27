a2c6c8a3e650d99d84efe3496d6dc3fcf312496c
George MacRorie <me@georgemac.com>
2023-01-27 12:01:34 +0000

    fix(fontawesome): set config.autoAddCss = false

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+
+config.autoAddCss = false
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>

fc7b22b79b2a47487053ce1f1c9ea270c3aa3fba
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-02-01 16:13:44 -0500

    chore: break into separate tests

--- tests/flags.spec.ts
+++ tests/flags.spec.ts
@@ -2,6 +2,7 @@ import { test } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('/');
+  await page.reload();
 });
 
 test('can create flag', async ({ page }) => {
@@ -11,58 +12,72 @@ test('can create flag', async ({ page }) => {
   await page.getByRole('button', { name: 'Create' }).click();
   await page.getByRole('switch', { name: 'Enabled' }).click();
   await page.getByRole('button', { name: 'Update' }).click();
-  await page.getByRole('link', { name: 'Flags', exact: true }).click();
 });
 
-test('can update flag with variants', async ({ page }) => {
+test('can update flag', async ({ page }) => {
   await page.getByRole('link', { name: 'hello-test' }).click();
   await page.getByLabel('Description').click();
   await page.getByLabel('Description').fill("i'm a test");
   await page.getByRole('button', { name: 'Update' }).click();
-  await page.getByRole('button', { name: 'Add Variant' }).click();
-  await page
-    .getByRole('dialog', { name: 'New Variant' })
-    .locator('#key')
-    .click();
-  await page
-    .getByRole('dialog', { name: 'New Variant' })
-    .locator('#key')
-    .fill('chrome');
-  await page.getByRole('button', { name: 'Create' }).click();
-  await page.getByRole('button', { name: 'New Variant' }).click();
-  await page
-    .getByRole('dialog', { name: 'New Variant' })
-    .locator('#key')
-    .click();
-  await page
-    .getByRole('dialog', { name: 'New Variant' })
-    .locator('#key')
-    .fill('firefox');
-  await page.getByRole('button', { name: 'Create' }).click();
-  await page.getByRole('link', { name: 'Edit ,chrome' }).click();
-  await page
-    .getByRole('dialog', { name: 'Edit Variant' })
-    .locator('#description')
-    .click();
-  await page
-    .getByRole('dialog', { name: 'Edit Variant' })
-    .locator('#description')
-    .fill('chrome browser');
-  await page
-    .getByRole('dialog', { name: 'Edit Variant' })
-    .getByRole('button', { name: 'Update' })
-    .click();
-  await page.getByRole('link', { name: 'Edit ,firefox' }).click();
-  await page
-    .getByRole('dialog', { name: 'Edit Variant' })
-    .locator('#description')
-    .click();
-  await page
-    .getByRole('dialog', { name: 'Edit Variant' })
-    .locator('#description')
-    .fill('firefox browser');
-  await page
-    .getByRole('dialog', { name: 'Edit Variant' })
-    .getByRole('button', { name: 'Update' })
-    .click();
+});
+
+test('can add variants to flag', async ({ page }) => {
+  await test.step('add variant', async () => {
+    await page.getByRole('link', { name: 'hello-test' }).click();
+    await page.getByRole('button', { name: 'Add Variant' }).click();
+    await page
+      .getByRole('dialog', { name: 'New Variant' })
+      .locator('#key')
+      .click();
+    await page
+      .getByRole('dialog', { name: 'New Variant' })
+      .locator('#key')
+      .fill('chrome');
+    await page.getByRole('button', { name: 'Create' }).click();
+  });
+
+  await test.step('add another variant', async () => {
+    await page.getByRole('button', { name: 'New Variant' }).click();
+    await page
+      .getByRole('dialog', { name: 'New Variant' })
+      .locator('#key')
+      .click();
+    await page
+      .getByRole('dialog', { name: 'New Variant' })
+      .locator('#key')
+      .fill('firefox');
+    await page.getByRole('button', { name: 'Create' }).click();
+  });
+
+  await test.step('edit variant description', async () => {
+    await page.getByRole('link', { name: 'Edit ,chrome' }).click();
+    await page
+      .getByRole('dialog', { name: 'Edit Variant' })
+      .locator('#description')
+      .click();
+    await page
+      .getByRole('dialog', { name: 'Edit Variant' })
+      .locator('#description')
+      .fill('chrome browser');
+    await page
+      .getByRole('dialog', { name: 'Edit Variant' })
+      .getByRole('button', { name: 'Update' })
+      .click();
+  });
+
+  await test.step('edit other variant description', async () => {
+    await page.getByRole('link', { name: 'Edit ,firefox' }).click();
+    await page
+      .getByRole('dialog', { name: 'Edit Variant' })
+      .locator('#description')
+      .click();
+    await page
+      .getByRole('dialog', { name: 'Edit Variant' })
+      .locator('#description')
+      .fill('firefox browser');
+    await page
+      .getByRole('dialog', { name: 'Edit Variant' })
+      .getByRole('button', { name: 'Update' })
+      .click();
+  });
 });

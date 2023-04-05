738da21033528ae3c66faa771509025f70894f6c
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-04-04 20:01:19 -0400

    chore: add unit test workflow

--- src/utils/helpers.test.ts
+++ src/utils/helpers.test.ts
@@ -3,20 +3,27 @@ import { addNamespaceToPath } from './helpers';
 describe('addNamespaceToPath', () => {
   it('should return a path with the namespace key', () => {
     const path = '/';
-    const key = 'test';
+    const key = 'example';
     const result = addNamespaceToPath(path, key);
-    expect(result).toEqual('/namespaces/test');
+    expect(result).toEqual('/namespaces/example/');
   });
 
-  it('should leave a path with the namespace key alone', () => {
-    const path = '/namespaces/test';
-    const key = 'test';
+  it('should leave a path with the same namespace key alone', () => {
+    const path = '/namespaces/example';
+    const key = 'example';
     const result = addNamespaceToPath(path, key);
-    expect(result).toEqual('/namespaces/test');
+    expect(result).toEqual('/namespaces/example');
   });
 
   it('handles existing noun in path', () => {
     const path = '/segments';
+    const key = 'example';
+    const result = addNamespaceToPath(path, key);
+    expect(result).toEqual('/namespaces/example/segments');
+  });
+
+  it('handles existing noun in path with new key', () => {
+    const path = '/namespaces/example/segments';
     const key = 'test';
     const result = addNamespaceToPath(path, key);
     expect(result).toEqual('/namespaces/test/segments');
